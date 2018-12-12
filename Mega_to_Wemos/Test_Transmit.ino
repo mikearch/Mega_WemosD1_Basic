53371b29a54816e179986ef96401b082384d5bbe
Michael Tomasik <mstomasik@gmail.com>
2018-12-12 13:00:12 +0100

    added transmit arrays and test data

--- Mega_to_Wemos/Test_Transmit.ino
+++ Mega_to_Wemos/Test_Transmit.ino
@@ -10,14 +10,47 @@
   - any serial device attached to Serial port 1
   - Serial Monitor open on Serial port 0
 
+Array Index Keys
+arrayBooleam[]
+  0 status_Home = whether current status is Home or Away
 
+arrayFloat[]
+  0 temp_Bar = temperature in Bar
+  1 temp_HtOn = set temperature degC for Away Heat On
+
+arrayLongUs[]
+  0 time_HeatCycle = set time in seconds for HeatOn during Away
+  1 time_CycleRem = time in seconds remaining in curr HeatCycle
 
 */
+//Defines lengths of arrays, change when new pubsub data added
+const int arraySize_boolean = 1;
+const int arraySize_float = 2;
+const int arraySize_longUsInt = 2;
+
+//Defines arrays for pubsub
+float arrayFloat[arraySize_float];
+boolean arrayBoolean[arraySize_boolean];
+unsignedlong arrayLongUs[arraySize_longUsInt];
+
+//Array identifier Variables
+String arrayFloatVars ="f";
+String arrayBoolVars ="b";
+String arrayLongUSVars ="l";
+
+//Reporting Variables
+boolean status_Home = false;
+float temp_Bar = 21.9;
+float temp_HtOn = 10;
+unsignedlong time_HeatCycle = 7200;
+unsignedlong time_CycleRem = 2430;
 
 //************Transmit Variables******************************************
 String begOfLine = "<";
 String endOfLine = ">";
 String delim =",";
+
+/*
 //**********Heating Group 1 Transmit Codes*******************************
 String burnState = "a";             //burner on/off state
 String pumpState = "b";             //pump on/off state
@@ -28,6 +61,8 @@ String delayTime = "f";             //Delay time set point
 //**********Power Group Transmit Codes**********************************
 String powerStatus = "g";           //status of house power
 
+
+
 //**********Test Variables************************************************
 boolean burner = false;
 boolean pump = false;
@@ -36,8 +71,11 @@ boolean HHold = false;
 float mTemp = 10;
 int dTime = 120;
 boolean powerState= false;
+*/
+
 
 int c = 1;     //initializes counter
+int n = 0;     //initializes array print counter
 
 void setup() {
   // initialize both serial ports:
@@ -46,13 +84,41 @@ void setup() {
 }
 
 void loop() {
+//Float array variable transmision
+  for (n=0; n <= arraySize_float - 1; n++ ) {
+    Serial1.print (begOfLine);
+    Serial1.print (arrayFloatVars);
+    Serial1.print(delim);
+    Serial1.print(n);
+    Serial1.print(delim);
+    Serial1.print(arrayFloat[n]);
+    Serial1.print(endOfLine);
+  }
 
-  Serial1.print (begOfLine);
-  Serial1.print (c);
-  Serial1.print(delim);
-  Serial1.print(burnState);
-  Serial1.print(delim);
-  Serial1.print(burner);
+//Boolean array variable transmision
+  for (n=0; n <= arraySize_boolean - 1; n++ ) {
+    Serial1.print (begOfLine);
+    Serial1.print (arrayBoolVars);
+    Serial1.print(delim);
+    Serial1.print(n);
+    Serial1.print(delim);
+    Serial1.print(arrayBoolean[n]);
+    Serial1.print(endOfLine);
+  }
+
+//Unsigned Long array variable transmision
+  for (n=0; n <= arraySize_longUsInt - 1; n++ ) {
+    Serial1.print (begOfLine);
+    Serial1.print (arrayLongUSVars);
+    Serial1.print(delim);
+    Serial1.print(n);
+    Serial1.print(delim);
+    Serial1.print(arrayLongUs[n]);
+    Serial1.print(endOfLine);
+  }
+
+delay(35000);
+/*
   Serial1.print(delim);
   Serial1.print(indrTemp);
   Serial1.print(delim);
@@ -61,4 +127,5 @@ void loop() {
   Serial1.print(endOfLine);
   delay(5000);
   c = c+1;
+  */
 }
