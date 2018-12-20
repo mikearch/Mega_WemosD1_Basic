a9f9ce7b12bdebaeafd2682f9fad6e2d86a0acd5
Michael Tomasik <mstomasik@gmail.com>
2018-12-20 10:21:43 +0100

    added missing setup_wifi

--- Wemos_to_Mega/wifi.ino
+++ Wemos_to_Mega/wifi.ino
@@ -84,3 +84,20 @@ void callback(String topic, byte* message, unsigned int length) {
   Serial.println();
 */
 }
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
